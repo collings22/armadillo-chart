fb5b750c180ee3e6ba9e508463d836762345deef
Daniel Collings <collings8915@gmail.com>
2021-01-22 13:29:28 +0000

    Updates

---
+++ precache-manifest.e4f32025600d5d03ac4e49a1f0353fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1f4bb1157cf6b33b7f3b03caacb675",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c60bd7747ea8b145fd3",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "3a714eef18ffb031e14e",
+    "url": "./static/js/2.1765e4b0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1765e4b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c60bd7747ea8b145fd3",
+    "url": "./static/js/main.75e15ca7.chunk.js"
+  },
+  {
+    "revision": "a25df327cb94a0b227c2",
+    "url": "./static/js/runtime-main.bb756556.js"
+  }
+]);
